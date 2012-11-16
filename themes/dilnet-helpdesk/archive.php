4c9d8fe255000a1567ba3bc3e72d91a9492f6f01
JP <dorado.johnphilip@gmail.com>
2012-11-16 17:17:45 +0800

    Edited Category Part

--- themes/dilnet-helpdesk/archive.php
+++ themes/dilnet-helpdesk/archive.php
@@ -7,8 +7,8 @@
 				    <div id="main" class="eightcol first clearfix" role="main">
 				
 					    <?php if (is_category()) { ?>
-						    <h1 class="archive-title h2">
-							    <span><?php _e("Posts Categorized:", "bonestheme"); ?></span> <?php single_cat_title(); ?>
+						    <h1 class="archive-title h1">
+							    <span><?php _e("", "bonestheme"); ?></span> <?php single_cat_title(); ?>
 					    	</h1>
 					    
 					    <?php } elseif (is_tag()) { ?> 
@@ -48,9 +48,9 @@
 						    <header class="article-header">
 							
 							    <h3 class="h2"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
-							
+							<!--
 							    <p class="byline vcard"><?php _e("Posted", "bonestheme"); ?> <time class="updated" datetime="<?php echo the_time('Y-m-j'); ?>" pubdate><?php the_time('F jS, Y'); ?></time> <?php _e("by", "bonestheme"); ?> <span class="author"><?php the_author_posts_link(); ?></span> <span class="amp">&</span> <?php _e("filed under", "bonestheme"); ?> <?php the_category(', '); ?>.</p>
-						
+							-->
 						    </header> <!-- end article header -->
 					
 						    <section class="entry-content clearfix">
